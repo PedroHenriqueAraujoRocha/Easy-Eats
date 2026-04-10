3eac9caee3e1ebde7a4d991ad9277921aaebc6cd
João Vitor <joaovitorbatista0104@gmail.com>
2026-04-10 19:39:04 -0300

    pequenos ajustes

--- src/app/cadastroProduto/cadastroProduto.ts
+++ src/app/cadastroProduto/cadastroProduto.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-produto',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cadastroProduto.html',
-  styleUrls: ['./cadastroProduto.scss']
+  styleUrls: ['./cadastroProduto.scss'],
 })
 export class CadastroProdutoComponent {
+  private router = inject(Router);
   currentYear = new Date().getFullYear();
+
+  protected acessarRota(rota: string) {
+    this.router.navigate([rota]);
+  }
 
   form = new FormGroup({
     nome: new FormControl('', Validators.required),
     categoria: new FormControl('', Validators.required),
     preco: new FormControl('', Validators.required),
-    estoque: new FormControl('', Validators.required)
+    estoque: new FormControl('', Validators.required),
   });
 
   salvar() {
