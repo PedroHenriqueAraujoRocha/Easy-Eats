77afaeacaa54885e1baabdb6a7577796cd76e5c5
joao_vc_batista <joaovitorbatista0104@gmail.com>
2026-05-20 20:02:34 -0300

    removido comentários

--- src/app/utils/modal/modalProduto.ts
+++ src/app/utils/modal/modalProduto.ts
@@ -6,10 +6,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./modal-produto.scss']
 })
 export class ModalProdutoComponent {
-  // Recebe o produto clicado lá da tela de pedidos
   @Input() produto: any = null; 
-
-  // Emite eventos de volta para a tela de pedidos
   @Output() fechar = new EventEmitter<void>();
   @Output() adicionar = new EventEmitter<any>();
 
@@ -19,6 +16,6 @@ export class ModalProdutoComponent {
 
   adicionarProduto() {
     this.adicionar.emit(this.produto);
-    this.fecharModal(); // Fecha o modal após adicionar
+    this.fecharModal();
   }
 }
