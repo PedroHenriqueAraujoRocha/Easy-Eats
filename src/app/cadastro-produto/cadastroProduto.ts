0e4ab005962003abad50548929b5e677d9d4cae3
joao_vc_batista <joaovitorbatista0104@gmail.com>
2026-05-20 21:40:52 -0300

    adicionado modal de cadastro de produto

--- src/app/cadastro-produto/cadastroProduto.ts
+++ src/app/cadastro-produto/cadastroProduto.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Navbar } from '../../components/navbar';
 import { Router } from '@angular/router';
+import { ModalCadastroProduto } from '../../components/modal-cadastro-produto/modal-cadastro-produto';
 
 @Component({
   selector: 'app-cadastro-produto',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, Navbar],
+  imports: [CommonModule, ReactiveFormsModule, Navbar, ModalCadastroProduto],
   templateUrl: './cadastroProduto.html',
   styleUrls: ['./cadastroProduto.scss'],
 })
@@ -21,16 +22,32 @@ export class CadastroProdutoComponent {
     preco: new FormControl('', Validators.required),
     estoque: new FormControl('', Validators.required),
   });
+
+  produtoParaConfirmar: any = null;
+
   protected acessarRota(rota: string) {
     this.router.navigate([rota]);
   }
 
-  salvar() {
+  abrirModalConfirmacao() {
     if (this.form.valid) {
-      console.log('Dados do produto:', this.form.value);
-      alert('Produto salvo com sucesso!');
+      this.produtoParaConfirmar = {
+        ...this.form.value,
+        emoji: '📦'
+      };
     } else {
       alert('Preencha todos os campos corretamente!');
     }
+  }
+
+  fecharModal() {
+    this.produtoParaConfirmar = null;
+  }
+
+  finalizarCadastro(produtoConfirmado: any) {
+    console.log('Dados do produto salvos:', produtoConfirmado);
+    alert('Produto salvo com sucesso!');
+    this.form.reset();
+    this.fecharModal();
   }
 }
